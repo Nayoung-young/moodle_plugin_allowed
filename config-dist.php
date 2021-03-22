4043311652d410963f92dc8b7e8c33cd85219865
Nayoung-young <lina_ny@naver.com>
2021-03-22 19:56:09 +0900

    plugin added

--- config-dist.php
+++ config-dist.php
@@ -525,7 +525,7 @@ $CFG->admin = 'admin';
 // features (such as cancelling the plugin installation or upgrade) via the
 // server administration web interface.
 //
-//     $CFG->disableupdateautodeploy = false;
+     $CFG->disableupdateautodeploy = false;
 //
 // Use the following flag to disable the warning on the system notifications page
 // about present development libraries. This flag will not disable the warning within
